856441471e72935932a7d423a3f2e390c4ced3ea
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-05-28 16:26:07 -0400

    Changed arrow function to normal function. Moved global variables to the top. Add comments for readability.

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-console.info('JavaScript Properly Linked. Delete me when completed.');
-
-const getRandomNumber = (number) => {
-    return Math.floor(Math.random() * number) + 1;
-}
-
-//global variables
+// global variables
 let winnerAnnouncement = document.querySelector('h1');
 let randomNumber1 = getRandomNumber(6);
 let randomNumber2 = getRandomNumber(6);
-
-
-let playerOneDice = document.querySelector('.img1');
-playerOneDice.setAttribute("src", `./img/dice${randomNumber1}.png`);
-
 let playerTwoDice = document.querySelector('.img2');
+let playerOneDice = document.querySelector('.img1');
+
+// random number generator
+function getRandomNumber (number) {
+    return Math.floor(Math.random() * number) + 1;
+}
+
+// roll dice & apply images
+playerOneDice.setAttribute("src", `./img/dice${randomNumber1}.png`);
 playerTwoDice.setAttribute("src", `./img/dice${randomNumber2}.png`);
 
+// decide winner and announce
 if (randomNumber1 > randomNumber2) {
     winnerAnnouncement.innerText = 'PLAYER ONE WINS!';
 } else if (randomNumber1 < randomNumber2) {
