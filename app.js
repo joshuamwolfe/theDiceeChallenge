99c2e434f3ea79f357d4412dbd68c0bf8d729b8a
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-05-28 16:16:22 -0400

    Add conditional statement to announce winner.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const getRandomNumber = (number) => {
     return Math.floor(Math.random() * number) + 1;
 }
 
+//global variables
+let winnerAnnouncement = document.querySelector('h1');
 let randomNumber1 = getRandomNumber(6);
 let randomNumber2 = getRandomNumber(6);
 
@@ -14,3 +16,10 @@ playerOneDice.setAttribute("src", `./img/dice${randomNumber1}.png`);
 let playerTwoDice = document.querySelector('.img2');
 playerTwoDice.setAttribute("src", `./img/dice${randomNumber2}.png`);
 
+if (randomNumber1 > randomNumber2) {
+    winnerAnnouncement.innerText = 'PLAYER ONE WINS!';
+} else if (randomNumber1 < randomNumber2) {
+    winnerAnnouncement.innerText = 'PLAYER TWO WINS!';
+} else {
+    winnerAnnouncement.innerText = 'tied.';
+}
